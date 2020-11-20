2150110490b86711b72929a1d6d90aaaa467496c
janber <472347656@qq.com>
2020-11-20 16:01:15 +0800

    feat: 添加首页导航

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/',
+    name: 'Home',
+    component: () => import('./../views/Home')
+  },
+  {
     path: '/tagcanvas',
     name: 'TagCanvas',
     component: () => import('../views/TagCanvas')
---
+++ src/views/Home/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home-box">
+    <div class="nav-item" v-for="item in nav_list" :key="item.url"><span>{{item.title}}</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      nav_list: [
+        {
+          title: 'TagCanvas',
+          url: '/tagcanvas'
+        }
+      ]
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style src="./index.less" lang="less"></style>
